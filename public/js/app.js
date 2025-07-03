065d8c0f67cd1e80872e4ce510cf3e32b533617b
Rodrigo <contato.rodrigoduartemkt@gmail.com>
2025-07-03 14:16:26 -0300

    Corrigindo mensagem inicial: trocando carrinhos por inscrições

--- public/js/app.js
+++ public/js/app.js
@@ -220,9 +220,8 @@ class DomineVerificador {
 📱 Envie o número abaixo que você acha suspeito para verificarmos se faz parte da nossa equipe oficial.
 
 ⚠️ **ATENÇÃO IMPORTANTE:**
-❌ NÃO façam nenhum tipo de compra agora, pois ainda não abrimos os carrinhos!
-📅 Só iremos abrir os carrinhos na **segunda-feira, dia 7**.
-🚫 Não comprem nada antes disso!
+❌ NÃO façam nenhum tipo de compra agora, pois não abrimos as inscrições!
+📅 Só iremos abrir na **segunda-feira, dia 7**.
 
 💬 Para falar com nosso suporte oficial: **[CLIQUE AQUI](https://wa.me/+555596869527)**`;
 
